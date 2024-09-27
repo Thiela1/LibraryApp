5dc92a9110522e7a366fc6a6c579afffcd4eda7f
Pobuhi <bigshot654@gmail.com>
2024-09-27 01:51:42 -0400

    Setting up the layout for the base window

--- src/LibraryGui.java
+++ src/LibraryGui.java
@@ -1,21 +1,29 @@
-// import java.awt.event.ActionListener;
-// import java.awt.event.ActionEvent;
-
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 
 public class LibraryGui extends JFrame {
     
+    JLabel appHeader;
+
     LibraryGui() {
 
+        // App header
+        appHeader = new JLabel("Digital Library");
+        appHeader.setFont(new Font(null, Font.PLAIN, 25));
+        appHeader.setBounds(0, 0, 100, 50);
+        appHeader.setHorizontalAlignment(JLabel.CENTER);
+        appHeader.setOpaque(true);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(900, 600);
+        this.setSize(300, 450);
         this.setVisible(true);
+        this.add(appHeader, BorderLayout.NORTH);
 
     }
-
-    // @Override
-    // public void actionPerformed(ActionEvent e) {
-        
-    // }
 
 }
