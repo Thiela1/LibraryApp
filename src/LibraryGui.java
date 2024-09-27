e8aebce638f0aad6551de448916692d6fa0305da
Pobuhi <bigshot654@gmail.com>
2024-09-26 23:24:50 -0400

    added Gui Class

---
+++ src/LibraryGui.java
@@ -0,0 +1,19 @@
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JFrame;
+
+public class LibraryGUI extends JFrame implements ActionListener {
+    
+    LibraryGUI() {
+
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+
+}
