522b29664c2ae76196361e86c11999f8a3f92191
Pobuhi <bigshot654@gmail.com>
2024-09-27 23:04:37 -0400

    Further cleaning

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
